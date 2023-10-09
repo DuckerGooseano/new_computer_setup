276f2f8f90382707b92b2840f53ab813e57c305b
ducker gooseano <ducker.gooseano@outlook.com>
2023-10-09 17:16:27 +0300

    add setup of new computer script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # new_computer_setup
+
+## setup new computer script 
+
+copy and paste this at a command line - 
+
+```
+wget -O -  https://raw.githubusercontent.com/DuckerGooseano/new_computer_setup/main/setup_new_computer.sh | bash
+```
---
+++ setup_new_computer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "-----------------------"
+echo "Setting work environment" 
+echo "-----------------------"
+
+echo "-------------"
+echo "Setting up git"
+echo "-------------"
+git config --global user.email "ducker.gooseano@outlook.com"
+git config --global user.name "ducker gooseano"
+
+echo "-------------"
+echo "installing developer pacakges"
+echo "-------------"
+sudo apt install -y docker.io
+
+echo "-------------"
+echo "Setting up projects dir"
+echo "-------------"
+
+mkdir -p ~/projects
+cd ~/projects/ 
+
+
